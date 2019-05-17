53586cf33beecf2bb7d6a4f23ca2891d6597fe65
Christophe DAVID <christophe.david@onera.fr>
2019-05-17 16:54:20 +0200

    added copyright notices

--- tests/module_management/test_bundle_loader.py
+++ tests/module_management/test_bundle_loader.py
@@ -1,11 +1,26 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+
+#      This file is part of FAST : A framework for rapid Overall Aircraft Design
+#      Copyright (C) 2019  ONERA/ISAE
+#      FAST is free software: you can redistribute it and/or modify
+#      it under the terms of the GNU General Public License as published by
+#      the Free Software Foundation, either version 3 of the License, or
+#      (at your option) any later version.
+#      This program is distributed in the hope that it will be useful,
+#      but WITHOUT ANY WARRANTY; without even the implied warranty of
+#      MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#      GNU General Public License for more details.
+#      You should have received a copy of the GNU General Public License
+#      along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 import logging
 import os.path as pth
 
 import pelix
-from fastoad.module_management.bundle_loader import Loader
 from pelix.framework import FrameworkFactory
+
+from fastoad.module_management.bundle_loader import Loader
 
 _logger = logging.getLogger(__name__)
 """Logger for this module"""
