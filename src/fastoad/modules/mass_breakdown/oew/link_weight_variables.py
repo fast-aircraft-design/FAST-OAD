bcc4cf87060977619df8bb4de220a3ab38fbcd74
Christophe DAVID <christophe.david@onera.fr>
2019-05-24 15:18:01 +0200

    Linted link_weight_variables + refactoring

--- src/fastoad/modules/mass_breakdown/oew/link_weight_variables.py
+++ src/fastoad/modules/mass_breakdown/oew/link_weight_variables.py
@@ -1,5 +1,5 @@
 """
-    FAST - Copyright (c) 2016 ONERA ISAE
+Helper for estimation of Operating Empty Weight
 """
 
 #  This file is part of FAST : A framework for rapid Overall Aircraft Design
@@ -14,42 +14,61 @@
 #  GNU General Public License for more details.
 #  You should have received a copy of the GNU General Public License
 #  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+from typing import List
+
 import numpy as np
 from openmdao.core.explicitcomponent import ExplicitComponent
+from openmdao.vectors.vector import Vector
+
+
+def get_subpart_weight_variables(category_name: str,
+                                 subpart_names: List[str]):
+    """
+
+    :param category_name:
+    :param subpart_names:
+    :return: weight variable names by assembling category_name with each
+    element of subpart_names
+    """
+    return ['%s:%s' % (category_name, name) for name in subpart_names]
 
 
 class LinkWeightVariables(ExplicitComponent):
+    """ Computes overall weight from all weight computations
+
+    Gets outputs from all subsystems to output global weight:
+        - for each category (airframe, propulsion, systems, furniture, crew)
+        - for the entire aircraft, i.e. OEW
+    """
 
     def initialize(self):
-        airframe_names = ['A1', 'A2', 'A31', 'A32', 'A4', 'A51', 'A52', 'A6', 'A7']
+        airframe_names = ['A1', 'A2', 'A31', 'A32', 'A4', 'A51', 'A52', 'A6',
+                          'A7']
         propulsion_names = ['B1', 'B2', 'B3']
-        systems_names = ['C11', 'C12', 'C13', 'C21', 'C22', 'C23', 'C24', 'C25',
+        systems_names = ['C11', 'C12', 'C13', 'C21', 'C22', 'C23', 'C24',
+                         'C25',
                          'C26', 'C27', 'C3', 'C4', 'C51', 'C52', 'C6']
         furniture_names = ['D1', 'D2', 'D3', 'D4', 'D5']
 
-        self.options.declare('airframe_names', default=airframe_names, types=list)
-        self.options.declare('propulsion_names', default=propulsion_names, types=list)
-        self.options.declare('systems_names', default=systems_names, types=list)
-        self.options.declare('furniture_names', default=furniture_names, types=list)
+        self.options.declare('airframe_names', default=airframe_names,
+                             types=list)
+        self.options.declare('propulsion_names', default=propulsion_names,
+                             types=list)
+        self.options.declare('systems_names', default=systems_names,
+                             types=list)
+        self.options.declare('furniture_names', default=furniture_names,
+                             types=list)
 
     def setup(self):
-        self.airframe_names = self.options['airframe_names']
-        self.propulsion_names = self.options['propulsion_names']
-        self.systems_names = self.options['systems_names']
-        self.furniture_names = self.options['furniture_names']
+        airframe_names = self.options['airframe_names']
+        propulsion_names = self.options['propulsion_names']
+        systems_names = self.options['systems_names']
+        furniture_names = self.options['furniture_names']
 
-        for name in range(len(self.airframe_names)):
-            self.add_input('weight_airframe:' + self.airframe_names[name], val=np.nan)
-
-        for name in range(len(self.propulsion_names)):
-            self.add_input('weight_propulsion:' + self.propulsion_names[name], val=np.nan)
-
-        for name in range(len(self.systems_names)):
-            self.add_input('weight_systems:' + self.systems_names[name], val=np.nan)
-
-        for name in range(len(self.furniture_names)):
-            self.add_input('weight_furniture:' + self.furniture_names[name], val=np.nan)
-
+        self.__add_weight_inputs('weight_airframe', airframe_names)
+        self.__add_weight_inputs('weight_propulsion', propulsion_names)
+        self.__add_weight_inputs('weight_systems', systems_names)
+        self.__add_weight_inputs('weight_furniture', furniture_names)
         self.add_input('weight_crew:E', val=np.nan)
 
         self.add_output('weight_airframe:A')
@@ -58,37 +77,59 @@ class LinkWeightVariables(ExplicitComponent):
         self.add_output('weight_furniture:D')
         self.add_output('weight:OEW')
 
-    def compute(self, inputs, outputs):
-        # Airframe
-        weight_A = []
-        weight_B = []
-        weight_C = []
-        weight_D = []
+    def compute(self, inputs, outputs
+                , discrete_inputs=None, discrete_outputs=None):
+        weight_a = self.__compute_category_weight(inputs, 'weight_airframe',
+                                                  self.options[
+                                                      'airframe_names'])
+        weight_b = self.__compute_category_weight(inputs, 'weight_propulsion',
+                                                  self.options[
+                                                      'propulsion_names'])
+        weight_c = self.__compute_category_weight(inputs, 'weight_systems',
+                                                  self.options[
+                                                      'systems_names'])
+        weight_d = self.__compute_category_weight(inputs, 'weight_furniture',
+                                                  self.options[
+                                                      'furniture_names'])
+        weight_e = inputs['weight_crew:E']
 
-        for name in range(len(self.airframe_names)):
-            weight_A.append(inputs['weight_airframe:' + self.airframe_names[name]][0])
+        oew = weight_a + weight_b + weight_c + weight_d + weight_e
 
-        for name in range(len(self.propulsion_names)):
-            weight_B.append(inputs['weight_propulsion:' + self.propulsion_names[name]][0])
-
-        for name in range(len(self.systems_names)):
-            weight_C.append(inputs['weight_systems:' + self.systems_names[name]][0])
-
-        for name in range(len(self.furniture_names)):
-            weight_D.append(inputs['weight_furniture:' + self.furniture_names[name]][0])
-
-        # Crew
-        E = inputs['weight_crew:E'][0]
-
-        A = sum(weight_A)
-        B = sum(weight_B)
-        C = sum(weight_C)
-        D = sum(weight_D)
-
-        oew = A + B + C + D + E
-
-        outputs['weight_airframe:A'] = A
-        outputs['weight_propulsion:B'] = B
-        outputs['weight_systems:C'] = C
-        outputs['weight_furniture:D'] = D
+        outputs['weight_airframe:A'] = weight_a
+        outputs['weight_propulsion:B'] = weight_b
+        outputs['weight_systems:C'] = weight_c
+        outputs['weight_furniture:D'] = weight_d
         outputs['weight:OEW'] = oew
+
+    def __add_weight_inputs(self, category_name: str,
+                            subpart_names: List[str]):
+        """
+        Populates self.inputs with weight variables
+
+        Variable names are built from category_name and each element of
+        subpart_names
+
+        :param category_name:
+        :param subpart_names:
+        """
+        for name in get_subpart_weight_variables(category_name, subpart_names):
+            self.add_input(name, val=np.nan)
+
+    @staticmethod
+    def __compute_category_weight(inputs: Vector, category_name: str,
+                                  subpart_names: List[str]):
+        """
+        Sums the weights of all subparts of the category
+
+        It assumes that weight of each subpart is in inputs with a
+        variable name like 'my_category:my_subpart_name'
+
+        :type inputs:
+        :param category_name:
+        :param subpart_names:
+
+        :return: sum of weights after iterating through subpart_names
+        """
+        input_ids = get_subpart_weight_variables(category_name,
+                                                 subpart_names)
+        return sum([inputs[input_id] for input_id in input_ids])
