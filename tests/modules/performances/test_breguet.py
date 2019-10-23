b14c188d4b3f1926b5adb800334de17bb9da9cba
Christophe DAVID <christophe.david@onera.fr>
2019-10-23 07:59:07 +0200

    added test of breguet+rubber_engine

--- tests/modules/performances/test_breguet.py
+++ tests/modules/performances/test_breguet.py
@@ -15,6 +15,7 @@ import openmdao.api as om
 from numpy.testing import assert_allclose
 
 from fastoad.modules.performances.breguet import Breguet
+from fastoad.modules.propulsion.fuel_engine.rubber_engine import OMRubberEngine
 from tests.testing_utilities import run_system
 
 
@@ -47,3 +48,30 @@ def test_breguet():
 
     assert_allclose(problem['MZFW'], 62473., rtol=1e-3)
     assert_allclose(problem['fuel_weight'], 11526., rtol=1e-3)
+
+
+def test_breguet_with_rubber_engine():
+    ivc = om.IndepVarComp()
+    ivc.add_output('altitude', 35000, units='ft')
+    ivc.add_output('mach', 0.78)
+    ivc.add_output('flight_distance', 500, units='NM')
+    ivc.add_output('lift_to_drag_ratio', 16.)
+    ivc.add_output('MTOW', 74000, units='kg')
+
+    ivc.add_output('bypass_ratio', 5)
+    ivc.add_output('maximum_mach', 0.95)
+    ivc.add_output('design_altitude', 35000, units='ft')
+    ivc.add_output('mto_thrust', 100000, units='N')
+    ivc.add_output('overall_pressure_ratio', 30)
+    ivc.add_output('phase', 3)
+    ivc.add_output('turbine_inlet_temperature', 1500, units='K')
+
+    group = om.Group()
+    group.add_subsystem('breguet', Breguet(), promotes=['*'])
+    group.add_subsystem('engine', OMRubberEngine(), promotes=['*'])
+    group.nonlinear_solver = om.NonlinearBlockGS()
+    problem = run_system(group, ivc)
+
+    # FIXME: obtained value are slighly different of legacy ones below
+    assert_allclose(problem['MZFW'], 65073., atol=1)
+    assert_allclose(problem['fuel_weight'], 8927., atol=1)
