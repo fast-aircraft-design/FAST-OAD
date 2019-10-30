fba1effab5fd51ef76dc3d03a08b304e8059b16b
Scott DELBECQ <scott.delbecq91@gmail.com>
2019-10-30 18:19:37 +0100

    Registered FAST OpenMDAO components

--- src/fastoad/modules/mass_breakdown/__init__.py
+++ src/fastoad/modules/mass_breakdown/__init__.py
@@ -13,3 +13,5 @@ Estimation of Aircraft Weight
 #  GNU General Public License for more details.
 #  You should have received a copy of the GNU General Public License
 #  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+from .mass_breakdown import MassBreakdown
