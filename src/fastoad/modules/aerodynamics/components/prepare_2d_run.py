Corrected knots units from 'kt' to 'kn' as defined by OpenMDAO

src/fastoad/modules/aerodynamics/components/prepare_2d_run.py
@@ -25,7 +25,7 @@ class Prepare2dRun(ExplicitComponent):
 
     def setup(self):
         self.add_input('geometry:wing_l0', val=np.nan, units='m')
-        self.add_input('tlar:v_approach', val=np.nan, units='kt')
+        self.add_input('tlar:v_approach', val=np.nan, units='kn')
         self.add_output('xfoil:reynolds')
         self.add_output('xfoil:mach')
 