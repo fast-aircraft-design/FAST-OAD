eba0dd6374bab678e29d6ee12a2dcb06613cbca4
Christophe DAVID <christophe.david@onera.fr>
2019-11-27 10:19:26 +0100

    Changed way to make weight sums for easier replacement of OpenMDAO vars

--- src/fastoad/modules/mass_breakdown/mass_breakdown.py
+++ src/fastoad/modules/mass_breakdown/mass_breakdown.py
@@ -1,7 +1,6 @@
 """
 Main components for mass breakdown
 """
-
 #  This file is part of FAST : A framework for rapid Overall Aircraft Design
 #  Copyright (C) 2019  ONERA/ISAE
 #  FAST is free software: you can redistribute it and/or modify
@@ -14,13 +13,8 @@ Main components for mass breakdown
 #  GNU General Public License for more details.
 #  You should have received a copy of the GNU General Public License
 #  along with this program.  If not, see <https://www.gnu.org/licenses/>.
-from typing import List
 
-import numpy as np
-from openmdao.core.explicitcomponent import ExplicitComponent
-from openmdao.solvers.linear.linear_block_gs import LinearBlockGS
-from openmdao.solvers.nonlinear.nonlinear_block_gs import NonlinearBlockGS
-from openmdao.vectors.vector import Vector
+import openmdao.api as om
 
 from fastoad.modules.mass_breakdown.a_airframe import WingWeight, FuselageWeight, EmpennageWeight, \
     FlightControlsWeight, LandingGearWeight, PylonsWeight, PaintWeight
@@ -57,10 +51,10 @@ class MassBreakdown(OpenMdaoOptionDispatcherGroup):
         super(MassBreakdown, self).configure()
 
         # Solvers setup
-        self.nonlinear_solver = NonlinearBlockGS()
+        self.nonlinear_solver = om.NonlinearBlockGS()
         self.nonlinear_solver.options['iprint'] = 0
         self.nonlinear_solver.options['maxiter'] = 50
-        self.linear_solver = LinearBlockGS()
+        self.linear_solver = om.LinearBlockGS()
         self.linear_solver.options['iprint'] = 0
 
 
@@ -109,107 +103,76 @@ class OperatingEmptyWeight(OpenMdaoOptionDispatcherGroup):
         self.add_subsystem('toilets_weight', ToiletsWeight(), promotes=['*'])
         # Crew
         self.add_subsystem('crew_weight', CrewWeight(), promotes=['*'])
-        self.add_subsystem('link_variables', LinkWeightVariables(), promotes=['*'])
 
+        # Make additions
+        self.add_subsystem('airframe_weight_sum',
+                           om.AddSubtractComp('weight_airframe:A',
+                                              ['weight_airframe:A1',
+                                               'weight_airframe:A2',
+                                               'weight_airframe:A31',
+                                               'weight_airframe:A32',
+                                               'weight_airframe:A4',
+                                               'weight_airframe:A51',
+                                               'weight_airframe:A52',
+                                               'weight_airframe:A6',
+                                               'weight_airframe:A7'],
+                                              units='kg',
+                                              desc='Mass of airframe'),
+                           promotes=['*']
+                           )
 
-class LinkWeightVariables(ExplicitComponent):
-    """ Computes overall weight from all weight computations
+        self.add_subsystem('propulsion_weight_sum',
+                           om.AddSubtractComp('weight_propulsion:B',
+                                              ['weight_propulsion:B1',
+                                               'weight_propulsion:B2',
+                                               'weight_propulsion:B3'],
+                                              units='kg',
+                                              desc='Mass of the propulsion system'),
+                           promotes=['*']
+                           )
 
-    Gets outputs from all subsystems to output global weight:
-        - for each category (airframe, propulsion, systems, furniture, crew)
-        - for the entire aircraft, i.e. OEW
-    """
+        self.add_subsystem('systems_weight_sum',
+                           om.AddSubtractComp('weight_systems:C',
+                                              ['weight_systems:C11',
+                                               'weight_systems:C12',
+                                               'weight_systems:C13',
+                                               'weight_systems:C21',
+                                               'weight_systems:C22',
+                                               'weight_systems:C23',
+                                               'weight_systems:C24',
+                                               'weight_systems:C25',
+                                               'weight_systems:C26',
+                                               'weight_systems:C27',
+                                               'weight_systems:C3',
+                                               'weight_systems:C4',
+                                               'weight_systems:C51',
+                                               'weight_systems:C52',
+                                               'weight_systems:C6'],
+                                              units='kg',
+                                              desc='Mass of aircraft systems'),
+                           promotes=['*']
+                           )
 
-    def initialize(self):
-        # TODO: decide if options are the correct way to handle these lists
-        airframe_names = ['A1', 'A2', 'A31', 'A32', 'A4', 'A51', 'A52', 'A6', 'A7']
-        propulsion_names = ['B1', 'B2', 'B3']
-        systems_names = ['C11', 'C12', 'C13', 'C21', 'C22', 'C23', 'C24', 'C25', 'C26', 'C27',
-                         'C3', 'C4', 'C51', 'C52', 'C6']
-        furniture_names = ['D1', 'D2', 'D3', 'D4', 'D5']
+        self.add_subsystem('furniture_weight_sum',
+                           om.AddSubtractComp('weight_furniture:D',
+                                              ['weight_furniture:D1',
+                                               'weight_furniture:D2',
+                                               'weight_furniture:D3',
+                                               'weight_furniture:D4',
+                                               'weight_furniture:D5'],
+                                              units='kg',
+                                              desc='Mass of aircraft furniture'),
+                           promotes=['*']
+                           )
 
-        self.options.declare('airframe_names', default=airframe_names, types=list)
-        self.options.declare('propulsion_names', default=propulsion_names, types=list)
-        self.options.declare('systems_names', default=systems_names, types=list)
-        self.options.declare('furniture_names', default=furniture_names, types=list)
-
-    def setup(self):
-        airframe_names = self.options['airframe_names']
-        propulsion_names = self.options['propulsion_names']
-        systems_names = self.options['systems_names']
-        furniture_names = self.options['furniture_names']
-
-        self.__add_weight_inputs('weight_airframe', airframe_names)
-        self.__add_weight_inputs('weight_propulsion', propulsion_names)
-        self.__add_weight_inputs('weight_systems', systems_names)
-        self.__add_weight_inputs('weight_furniture', furniture_names)
-        self.add_input('weight_crew:E', val=np.nan, units='kg')
-
-        self.add_output('weight_airframe:A', units='kg')
-        self.add_output('weight_propulsion:B', units='kg')
-        self.add_output('weight_systems:C', units='kg')
-        self.add_output('weight_furniture:D', units='kg')
-        self.add_output('weight:OEW', units='kg')
-
-    def compute(self, inputs, outputs
-                , discrete_inputs=None, discrete_outputs=None):
-        weight_a = self.__compute_category_weight(inputs, 'weight_airframe',
-                                                  self.options['airframe_names'])
-        weight_b = self.__compute_category_weight(inputs, 'weight_propulsion',
-                                                  self.options['propulsion_names'])
-        weight_c = self.__compute_category_weight(inputs, 'weight_systems',
-                                                  self.options['systems_names'])
-        weight_d = self.__compute_category_weight(inputs, 'weight_furniture',
-                                                  self.options['furniture_names'])
-        weight_e = inputs['weight_crew:E']
-
-        oew = weight_a + weight_b + weight_c + weight_d + weight_e
-
-        outputs['weight_airframe:A'] = weight_a
-        outputs['weight_propulsion:B'] = weight_b
-        outputs['weight_systems:C'] = weight_c
-        outputs['weight_furniture:D'] = weight_d
-        outputs['weight:OEW'] = oew
-
-    def __add_weight_inputs(self, category_name: str,
-                            subpart_names: List[str]):
-        """
-        Populates self.inputs with weight variables
-
-        Variable names are built from category_name and each element of
-        subpart_names
-
-        :param category_name:
-        :param subpart_names:
-        """
-        for name in self.__get_subpart_weight_variables(category_name, subpart_names):
-            self.add_input(name, val=np.nan, units='kg')
-
-    @classmethod
-    def __compute_category_weight(cls, inputs: Vector, category_name: str,
-                                  subpart_names: List[str]):
-        """
-        Sums the weights of all sub-parts of the category
-
-        It assumes that weight of each subpart is in inputs with a
-        variable name like 'my_category:my_subpart_name'
-
-        :type inputs:
-        :param category_name:
-        :param subpart_names:
-
-        :return: sum of weights after iterating through subpart_names
-        """
-        input_ids = cls.__get_subpart_weight_variables(category_name, subpart_names)
-        return sum([inputs[input_id] for input_id in input_ids])
-
-    @staticmethod
-    def __get_subpart_weight_variables(category_name: str, subpart_names: List[str]):
-        """
-
-        :param category_name:
-        :param subpart_names:
-        :return: weight variable names by assembling category_name with each
-        element of subpart_names
-        """
-        return ['%s:%s' % (category_name, name) for name in subpart_names]
+        self.add_subsystem('OWE_sum',
+                           om.AddSubtractComp('weight:OEW',
+                                              ['weight_airframe:A',
+                                               'weight_propulsion:B',
+                                               'weight_systems:C',
+                                               'weight_furniture:D',
+                                               'weight_crew:E'],
+                                              units='kg',
+                                              desc='Mass of crew'),
+                           promotes=['*']
+                           )
