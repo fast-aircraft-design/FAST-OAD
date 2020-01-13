135e94b499f9164c54419016a6fd0a1f013ea9b3
Christophe DAVID <christophe.david@onera.fr>
2020-01-13 15:14:54 +0100

    Fixed crashes that happened when trying to install from a tarball
    setuptool_scm was raising an error because it could not get the version
    from the .git folder that is not in tarballs.

--- setup.py
+++ setup.py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 """
     Setup file for fastoad.
     Use setup.cfg to configure your project.
@@ -31,6 +30,6 @@ except VersionConflict:
     print("Error: version of setuptools is too old (<38.3)!")
     sys.exit(1)
 
-
 if __name__ == "__main__":
-    setup(use_pyscaffold=True)
+    # Using fallback_version helps not crashing when installing from tarball
+    setup(use_pyscaffold={'fallback_version': '0.0.0'})
