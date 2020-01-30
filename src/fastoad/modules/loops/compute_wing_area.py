ab4245ec775c726dcc8efca4af5161f06e6e0bf1
Christophe DAVID <christophe.david@onera.fr>
2020-01-30 11:36:33 +0100

    Added computation of check/constraint variables
    * MFW - fuel for sizing mission
    * CLmax - max CL needed for landing

--- src/fastoad/modules/loops/compute_wing_area.py
+++ src/fastoad/modules/loops/compute_wing_area.py
@@ -18,7 +18,14 @@ import numpy as np
 import openmdao.api as om
 
 
-class ComputeWingArea(om.ExplicitComponent):
+class ComputeWingArea(om.Group):
+
+    def setup(self):
+        self.add_subsystem('wing_area', _ComputeWingArea(), promotes=['*'])
+        self.add_subsystem('constraints', ComputeWingAreaConstraints(), promotes=['*'])
+
+
+class _ComputeWingArea(om.ExplicitComponent):
     """ Computation of wing area from needed approach speed and mission fuel """
 
     def setup(self):
@@ -50,3 +57,38 @@ class ComputeWingArea(om.ExplicitComponent):
         wing_area_approach = 2 * mlw * 9.81 / ((approach_speed / 1.23) ** 2) / (1.225 * max_CL)
 
         outputs['geometry:wing:area'] = max(wing_area_mission, wing_area_approach)
+
+
+class ComputeWingAreaConstraints(om.ExplicitComponent):
+
+    def setup(self):
+        self.add_input('mission:sizing:fuel', val=np.nan, units='kg')
+        self.add_input('weight:aircraft:MFW', val=np.nan, units='kg')
+
+        self.add_input('TLAR:approach_speed', val=np.nan, units='m/s')
+        self.add_input('weight:aircraft:MLW', val=np.nan, units='kg')
+        self.add_input('aerodynamics:aircraft:landing:CL_max', val=np.nan)
+        self.add_input('geometry:wing:area', val=np.nan, units='m**2')
+
+        self.add_output('weight:aircraft:additional_fuel_capacity', units='kg')
+        self.add_output('aerodynamics:aircraft:landing:additional_CL_capacity')
+
+        self.declare_partials('weight:aircraft:additional_fuel_capacity',
+                              ['weight:aircraft:MFW', 'mission:sizing:fuel'],
+                              method='fd')
+        self.declare_partials('aerodynamics:aircraft:landing:additional_CL_capacity',
+                              ['TLAR:approach_speed', 'weight:aircraft:MLW',
+                               'aerodynamics:aircraft:landing:CL_max',
+                               'geometry:wing:area'], method='fd')
+
+    def compute(self, inputs, outputs):
+        mfw = inputs['weight:aircraft:MFW']
+        mission_fuel = inputs['mission:sizing:fuel']
+        v_approach = inputs['TLAR:approach_speed']
+        cl_max = inputs['aerodynamics:aircraft:landing:CL_max']
+        mlw = inputs['weight:aircraft:MLW']
+        wing_area = inputs['geometry:wing:area']
+
+        outputs['weight:aircraft:additional_fuel_capacity'] = mfw - mission_fuel
+        outputs['aerodynamics:aircraft:landing:additional_CL_capacity'] = cl_max - mlw * 9.81 / (
+                0.5 * 1.225 * (v_approach / 1.23) ** 2 * wing_area)
