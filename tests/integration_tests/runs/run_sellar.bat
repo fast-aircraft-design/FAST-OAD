0923851ad1784592f724ced4a2aab65e11d373b1
Christophe DAVID <christophe.david@onera.fr>
2020-01-31 07:49:29 +0100

    Removed "runs" directory that is no more used

--- tests/integration_tests/runs/run_sellar.bat
+++
@@ -1,9 +0,0 @@
-REM To have fast.exe ready, do "python setup.py install_scripts"
-REM and set your path accordingly (not needed in PyCharm terminal)
-
-
-REM generate inputs.xml
-fast.exe conf_sellar.toml gen_inputs
-
-REM run the sellar problem
-fast.exe conf_sellar.toml run_model
