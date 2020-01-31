e0d4cf197235fa779d80c3bdf45ca576539a1483
Christophe DAVID <christophe.david@onera.fr>
2020-01-31 08:34:28 +0100

    added non-regression test to compare with legacy

--- tests/integration_tests/oad_process/test_oad_process.py
+++ tests/integration_tests/oad_process/test_oad_process.py
@@ -136,8 +136,8 @@ def test_oad_process(cleanup, install_components):
 
 
 def test_api(cleanup, install_components):
-    api_result_folder_path = pth.join(RESULTS_FOLDER_PATH, 'api')
-    configuration_file_path = pth.join(api_result_folder_path, 'oad_process.toml')
+    results_folder_path = pth.join(RESULTS_FOLDER_PATH, 'api')
+    configuration_file_path = pth.join(results_folder_path, 'oad_process.toml')
 
     # Generation of configuration file ----------------------------------------
     api.generate_configuration_file(configuration_file_path, True)
@@ -146,7 +146,7 @@ def test_api(cleanup, install_components):
     # We get the same inputs as in tutorial notebook
     source_xml = pth.join(root_folder_path, 'src', 'fastoad',
                           'notebooks', 'tutorial', 'data', 'CeRAS01_baseline.xml')
-    api.generate_inputs(configuration_file_path, source_xml, True)
+    api.generate_inputs(configuration_file_path, source_xml, overwrite=True)
 
     # Run model ---------------------------------------------------------------
     problem = api.evaluate_problem(configuration_file_path, True)
@@ -171,7 +171,7 @@ def test_api(cleanup, install_components):
     ht_area = problem['geometry:horizontal_tail:area']
     vt_area = problem['geometry:vertical_tail:area']
 
-    # Run model ---------------------------------------------------------------
+    # Run optim ---------------------------------------------------------------
     problem = api.optimize_problem(configuration_file_path, True)
 
     # Check that weight-performances loop correctly converged
@@ -202,3 +202,63 @@ def test_api(cleanup, install_components):
     print(problem['weight:aircraft:MTOW'])
     print(problem['geometry:wing:area'])
     print(problem['geometry:vertical_tail:area'])
+
+
+def test_non_regression(cleanup, install_components):
+    results_folder_path = pth.join(RESULTS_FOLDER_PATH, 'non_regression')
+    configuration_file_path = pth.join(results_folder_path, 'oad_process.toml')
+
+    # Generation of configuration file ----------------------------------------
+    api.generate_configuration_file(configuration_file_path, True)
+
+    # Generation of inputs ----------------------------------------------------
+    # We get the same inputs as in tutorial notebook
+    source_xml = pth.join(DATA_FOLDER_PATH, 'CeRAS01_legacy.xml')
+    api.generate_inputs(configuration_file_path, source_xml, source_path_schema='legacy',
+                        overwrite=True)
+
+    # Run model ---------------------------------------------------------------
+    problem = api.evaluate_problem(configuration_file_path, True)
+    om.view_connections(problem, outfile=pth.join(results_folder_path, 'connections.html'),
+                        show_browser=False)
+
+    # Check that weight-performances loop correctly converged
+    assert_allclose(problem['weight:aircraft:OWE'],
+                    problem['weight:airframe:mass'] + problem['weight:propulsion:mass']
+                    + problem['weight:systems:mass'] + problem['weight:furniture:mass']
+                    + problem['weight:crew:mass'],
+                    atol=1)
+    assert_allclose(problem['weight:aircraft:MZFW'],
+                    problem['weight:aircraft:OWE'] + problem['weight:aircraft:max_payload'],
+                    atol=1)
+    assert_allclose(problem['weight:aircraft:MTOW'],
+                    problem['weight:aircraft:OWE'] + problem['weight:aircraft:payload']
+                    + problem['mission:sizing:mission:fuel'],
+                    atol=1)
+
+    ref_var_list = OMLegacy1XmlIO(
+        pth.join(DATA_FOLDER_PATH, 'CeRAS01_legacy_result.xml')).read_variables()
+
+    import pandas as pd
+    import numpy as np
+
+    row_list = []
+    for ref_var in ref_var_list:
+        try:
+            value = problem.get_val(ref_var.name, units=ref_var.units)[0]
+        except KeyError:
+            continue
+        row_list.append(
+            {'name': ref_var.name, 'units': ref_var.units,
+             'ref_value': ref_var.value[0], 'value': value}
+        )
+
+    df = pd.DataFrame(row_list)
+    df['delta [%]'] = (df['value'] - df['ref_value']) * 100 / df['ref_value']
+    df['delta [%]'][(df['ref_value'] == 0) & (df['value'] == 0)] = 0.
+    df['abs_delta [%]'] = np.abs(df['delta [%]'])
+
+    pd.set_option('display.max_rows', None)
+    pd.set_option('display.max_columns', None)
+    pd.set_option('display.width', 1000)
+    print(df.sort_values(by=['abs_delta [%]']))
