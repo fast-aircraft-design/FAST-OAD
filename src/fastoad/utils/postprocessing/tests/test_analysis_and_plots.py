9a7fabd712d1690f1ce0caeaf29d96e986ccad58
Scott Delbecq <scott.delbecq91@gmail.com>
2020-03-27 12:47:31 +0100

    Added test for aircraft top view plot

--- src/fastoad/utils/postprocessing/tests/test_analysis_and_plots.py
+++ src/fastoad/utils/postprocessing/tests/test_analysis_and_plots.py
@@ -18,7 +18,8 @@ import os.path as pth
 
 from fastoad.io.xml import OMXmlIO
 from fastoad.utils.postprocessing.analysis_and_plots import \
-    wing_geometry_plot, drag_polar_plot, mass_breakdown_plot
+    wing_geometry_plot, drag_polar_plot, mass_breakdown_plot, \
+    aircraft_geometry_plot
 
 DATA_FOLDER_PATH = pth.join(pth.dirname(__file__), 'data')
 
@@ -46,6 +47,31 @@ def test_wing_geometry_plot():
     # This is a rudimentary test as plot are difficult to verify
     # The test will fail if an error is raised by the following line
     fig = wing_geometry_plot(xml, name='Second plot', fig=fig)
+
+
+def test_aircraft_geometry_plot():
+    """
+    Basic tests for testing the plotting.
+    """
+
+    filename = pth.join(DATA_FOLDER_PATH, 'problem_outputs.xml')
+
+    xml = OMXmlIO(filename)
+
+    # First plot
+    # This is a rudimentary test as plot are difficult to verify
+    # The test will fail if an error is raised by the following line
+    fig = aircraft_geometry_plot(xml)
+
+    # First plot with name
+    # This is a rudimentary test as plot are difficult to verify
+    # The test will fail if an error is raised by the following line
+    fig = aircraft_geometry_plot(xml, name='First plot')
+
+    # Adding a plot to the previous fig
+    # This is a rudimentary test as plot are difficult to verify
+    # The test will fail if an error is raised by the following line
+    fig = aircraft_geometry_plot(xml, name='Second plot', fig=fig)
 
 
 def test_drag_polar_plot():
