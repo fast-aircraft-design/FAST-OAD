ed826d56536559d84d9cfec6e6d1df833385e9d8
Christophe DAVID <christophe.david@onera.fr>
2020-06-30 13:38:05 +0200

    Added ranged flight computation

--- src/fastoad/models/performances/mission/flight.py
+++ src/fastoad/models/performances/mission/flight.py
@@ -12,15 +12,15 @@
 #  You should have received a copy of the GNU General Public License
 #  along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
-from typing import Dict, List
+from typing import Dict
 
 import pandas as pd
 from fastoad.constants import FlightPhase, EngineSetting
 from fastoad.models.performances.mission.segments.acceleration import AccelerationSegment
-from fastoad.models.performances.mission.segments.base import AbstractSegment
 from fastoad.models.performances.mission.segments.cruise import OptimalCruiseSegment
 from fastoad.models.propulsion import IPropulsion
 from scipy.constants import foot, knot
+from scipy.optimize import root_scalar
 
 from ..mission.flight_point import FlightPoint
 from ..mission.polar import Polar
@@ -44,92 +44,132 @@ class Flight:
         cruise_distance: float,
     ):
 
-        segment_low_speed_args = propulsion, reference_surface, low_speed_polar
-        segment_high_speed_args = propulsion, reference_surface, high_speed_polar
+        self.segment_low_speed_args = propulsion, reference_surface, low_speed_polar
+        self.segment_high_speed_args = propulsion, reference_surface, high_speed_polar
+        self.cruise_mach = cruise_mach
+        self.thrust_rates = thrust_rates
+        self.cruise_distance = cruise_distance
 
-        self.flight_sequence: List[AbstractSegment] = [
-            # Initial climb
+    def get_flight_sequence(self):
+        return [
+            # Initial climb ====================================================
             ClimbDescentSegment(
                 FlightPoint(true_airspeed="constant", altitude=400.0 * foot),
-                *segment_low_speed_args,
+                *self.segment_low_speed_args,
                 thrust_rate=1.0,
                 engine_setting=EngineSetting.TAKEOFF,
             ),
             AccelerationSegment(
                 FlightPoint(equivalent_airspeed=250.0 * knot),
-                *segment_low_speed_args,
+                *self.segment_low_speed_args,
                 thrust_rate=1.0,
                 engine_setting=EngineSetting.TAKEOFF,
             ),
             ClimbDescentSegment(
                 FlightPoint(equivalent_airspeed="constant", altitude=1500.0 * foot),
-                *segment_low_speed_args,
+                *self.segment_low_speed_args,
                 thrust_rate=1.0,
                 engine_setting=EngineSetting.TAKEOFF,
             ),
-            # Climb
+            # Climb ============================================================
             ClimbDescentSegment(
                 FlightPoint(equivalent_airspeed="constant", altitude=10000.0 * foot),
-                *segment_high_speed_args,
-                thrust_rate=thrust_rates[FlightPhase.CLIMB],
+                *self.segment_high_speed_args,
+                thrust_rate=self.thrust_rates[FlightPhase.CLIMB],
                 engine_setting=EngineSetting.CLIMB,
             ),
             AccelerationSegment(
                 FlightPoint(equivalent_airspeed=300.0 * knot),
-                *segment_high_speed_args,
-                thrust_rate=thrust_rates[FlightPhase.CLIMB],
+                *self.segment_high_speed_args,
+                thrust_rate=self.thrust_rates[FlightPhase.CLIMB],
                 engine_setting=EngineSetting.CLIMB,
             ),
             ClimbDescentSegment(
                 FlightPoint(
                     equivalent_airspeed="constant", altitude=ClimbDescentSegment.OPTIMAL_ALTITUDE
                 ),
-                *segment_high_speed_args,
-                thrust_rate=thrust_rates[FlightPhase.CLIMB],
-                cruise_mach=cruise_mach,
+                *self.segment_high_speed_args,
+                thrust_rate=self.thrust_rates[FlightPhase.CLIMB],
+                cruise_mach=self.cruise_mach,
                 engine_setting=EngineSetting.CLIMB,
             ),
-            # Cruise
+            # Cruise ===========================================================
             OptimalCruiseSegment(
-                FlightPoint(ground_distance=cruise_distance),
-                *segment_high_speed_args,
-                cruise_mach=cruise_mach,
+                FlightPoint(ground_distance=self.cruise_distance),
+                *self.segment_high_speed_args,
+                cruise_mach=self.cruise_mach,
                 engine_setting=EngineSetting.CRUISE,
             ),
-            # Descent
+            # Descent ==========================================================
             ClimbDescentSegment(
                 FlightPoint(equivalent_airspeed=300.0 * knot, mach="constant"),
-                *segment_high_speed_args,
-                thrust_rate=thrust_rates[FlightPhase.DESCENT],
+                *self.segment_high_speed_args,
+                thrust_rate=self.thrust_rates[FlightPhase.DESCENT],
                 engine_setting=EngineSetting.IDLE,
             ),
             # ClimbDescentSegment(
             #     FlightPoint(altitude=10000.0 * foot, equivalent_airspeed="constant"),
-            #     *segment_high_speed_args,
-            #     thrust_rate=thrust_rates[FlightPhase.DESCENT],
+            #     *self.segment_high_speed_args,
+            #     thrust_rate=self.thrust_rates[FlightPhase.DESCENT],
             #     engine_setting=EngineSetting.IDLE,
             # ),
             AccelerationSegment(
                 FlightPoint(equivalent_airspeed=250.0 * knot),
-                *segment_high_speed_args,
-                thrust_rate=thrust_rates[FlightPhase.DESCENT],
+                *self.segment_high_speed_args,
+                thrust_rate=self.thrust_rates[FlightPhase.DESCENT],
                 engine_setting=EngineSetting.IDLE,
             ),
             ClimbDescentSegment(
                 FlightPoint(altitude=1500.0 * foot, equivalent_airspeed="constant"),
-                *segment_low_speed_args,
-                thrust_rate=thrust_rates[FlightPhase.DESCENT],
+                *self.segment_low_speed_args,
+                thrust_rate=self.thrust_rates[FlightPhase.DESCENT],
                 engine_setting=EngineSetting.IDLE,
             ),
         ]
 
     def compute(self, start: FlightPoint) -> pd.DataFrame:
+        flight_sequence = self.get_flight_sequence()
         segment_start = start
         segments = []
-        for segment_calculator in self.flight_sequence:
+        for segment_calculator in flight_sequence:
             print(segment_calculator)
             segments.append(segment_calculator.compute(segment_start))
             segment_start = FlightPoint(segments[-1].iloc[-1])
             print(segment_start)
 
         return pd.concat(segments)
+
+
+class RangedFlight(Flight):
+    def __init__(
+        self,
+        propulsion: IPropulsion,
+        reference_surface: float,
+        low_speed_polar: Polar,
+        high_speed_polar: Polar,
+        cruise_mach: float,
+        thrust_rates: Dict[FlightPhase, float],
+        range: float,
+    ):
+        self.range = range
+        self.flight = Flight(
+            propulsion,
+            reference_surface,
+            low_speed_polar,
+            high_speed_polar,
+            cruise_mach,
+            thrust_rates,
+            range,
+        )
+        self.flight_points = None
+
+    def compute(self, start: FlightPoint) -> pd.DataFrame:
+        def compute_flight(cruise_distance):
+            self.flight.cruise_distance = cruise_distance
+            self.flight_points = self.flight.compute(start)
+            obtained_range = self.flight_points.iloc[-1].ground_distance
+            return self.range - obtained_range
+
+        needed_cruise_range = root_scalar(compute_flight, x0=self.range, x1=self.range * 0.5).root
+        print(needed_cruise_range)
+        return self.flight_points
