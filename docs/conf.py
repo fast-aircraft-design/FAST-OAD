5e945c9b91930e32da19e3ef86d56621bc1b6e3c
Christophe DAVID <christophe.david@onera.fr>
2020-08-26 16:35:57 +0200

    Excluded notebooks from automatic documentation.

--- docs/conf.py
+++ docs/conf.py
@@ -68,7 +68,14 @@ templates_path = ["_templates"]
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = ["_build", "Thumbs.db", ".DS_Store", "api/*.tests.*", "api/*.resources.rst"]
+exclude_patterns = [
+    "_build",
+    "Thumbs.db",
+    ".DS_Store",
+    "api/*.tests.*",
+    "api/*.resources.rst",
+    "api/fastoad.notebooks.*rst",
+]
 
 # Use docstring from class and __init__
 autoclass_content = "both"
