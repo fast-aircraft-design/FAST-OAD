7c1ae76a553b326d6187109ddb533d3f04abe287
Christophe DAVID <christophe.david@onera.fr>
2020-11-26 15:54:12 +0100

    Generic mission module now tested in "mission_only" integration test

--- src/fastoad/register.py
+++ src/fastoad/register.py
@@ -24,6 +24,7 @@ from fastoad.models.handling_qualities.compute_static_margin import ComputeStati
 from fastoad.models.handling_qualities.tail_sizing.compute_tail_areas import ComputeTailAreas
 from fastoad.models.loops.compute_wing_area import ComputeWingArea
 from fastoad.models.performances.breguet import OMBreguet
+from fastoad.models.performances.mission.openmdao.mission import Mission
 from fastoad.models.performances.mission.openmdao.sizing_mission import SizingMission
 from fastoad.models.propulsion.fuel_propulsion.rubber_engine import (
     OMRubberEngineComponent,
@@ -86,6 +87,10 @@ def register_openmdao_systems():
 
     RegisterOpenMDAOSystem("fastoad.performances.sizing_mission", domain=ModelDomain.PERFORMANCE)(
         SizingMission
+    )
+
+    OpenMDAOSystemRegistry.register_system(
+        Mission, "fastoad.performances.mission", domain=ModelDomain.PERFORMANCE
     )
 
     # Propulsion ##################################################################
