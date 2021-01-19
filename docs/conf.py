f67155ba0685df1bab88fb2600d5d9b9190fa0da
Christophe DAVID <christophe.david@onera.fr>
2021-01-19 14:53:21 +0100

    Adapted Sphinx conf for bibtex 2.0

--- docs/conf.py
+++ docs/conf.py
@@ -74,6 +74,7 @@ extensions = [
     "sphinx.ext.napoleon",
     "sphinxcontrib.bibtex",
 ]
+bibtex_bibfiles = ["refs.bib"]
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ["_templates"]
