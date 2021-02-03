49b0f9ac4a7d53f4c7911436d89c94c8d2918850
Christophe DAVID <christophe.david@onera.fr>
2021-02-03 16:41:18 +0100

    Added test for plugin management

---
+++ src/fastoad/module_management/tests/data/dummy_plugin/declared_dummy_components.py
@@ -0,0 +1,26 @@
+"""
+Module where services are declared externally (in the __init__.py)
+"""
+#  This file is part of FAST-OAD : A framework for rapid Overall Aircraft Design
+#  Copyright (C) 2021 ONERA & ISAE-SUPAERO
+#  FAST is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#  You should have received a copy of the GNU General Public License
+#  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+
+from ...test_plugins import DummyBase
+
+
+class DeclaredDummy1(DummyBase):
+    pass
+
+
+class DeclaredDummy2(DummyBase):
+    pass
