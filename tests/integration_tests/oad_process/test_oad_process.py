d2488635eacdc5dad0ce79a5bf94daab859c74bc
Scott Delbecq <scott.delbecq91@gmail.com>
2021-03-11 08:33:07 +0100

    Updated the default optimization problem and updated the tutorial notebooks

--- tests/integration_tests/oad_process/test_oad_process.py
+++ tests/integration_tests/oad_process/test_oad_process.py
@@ -301,10 +301,10 @@ def test_api(cleanup):
     )
 
     # Design Variable
-    assert_allclose(problem["data:geometry:wing:aspect_ratio"], 9.92, atol=1e-2)
+    assert_allclose(problem["data:geometry:wing:aspect_ratio"], 14.24, atol=1e-2)
 
     # Constraint
-    assert_allclose(problem["data:geometry:wing:span"], 36.0, atol=1e-1)
+    assert_allclose(problem["data:geometry:wing:span"], 44.9, atol=1e-1)
 
     # Objective
-    assert_allclose(problem["data:mission:sizing:fuel"], 20224, atol=50)
+    assert_allclose(problem["data:mission:sizing:fuel"], 19862, atol=50)
