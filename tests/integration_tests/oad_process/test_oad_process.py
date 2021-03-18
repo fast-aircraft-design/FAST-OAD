e31628e5a89a263fb98fdb253c4041c8cfb507ea
Christophe DAVID <christophe.david@onera.fr>
2021-03-18 10:38:37 +0100

    Renamed folders and CeRAS notebook

--- tests/integration_tests/oad_process/test_oad_process.py
+++ tests/integration_tests/oad_process/test_oad_process.py
@@ -248,7 +248,13 @@ def test_api_eval(cleanup):
     # Generation of inputs ----------------------------------------------------
     # We get the same inputs as in tutorial notebook
     source_xml = pth.join(
-        root_folder_path, "src", "fastoad", "notebooks", "tutorial", "data", "CeRAS01_baseline.xml"
+        root_folder_path,
+        "src",
+        "fastoad",
+        "notebooks",
+        "01_tutorial",
+        "data",
+        "CeRAS01_baseline.xml",
     )
     api.generate_inputs(configuration_file_path, source_xml, overwrite=True)
 
@@ -297,7 +303,13 @@ def test_api_optim(cleanup):
     # Generation of inputs ----------------------------------------------------
     # We get the same inputs as in tutorial notebook
     source_xml = pth.join(
-        root_folder_path, "src", "fastoad", "notebooks", "tutorial", "data", "CeRAS01_baseline.xml"
+        root_folder_path,
+        "src",
+        "fastoad",
+        "notebooks",
+        "01_tutorial",
+        "data",
+        "CeRAS01_baseline.xml",
     )
     api.generate_inputs(configuration_file_path, source_xml, overwrite=True)
 
