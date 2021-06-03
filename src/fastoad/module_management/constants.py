1121f6e3534fd99496aec571e3d91c24313e896f
Christophe DAVID <christophe.david@onera.fr>
2021-06-03 09:11:34 +0200

    ModelDomain is now extensible.

--- src/fastoad/module_management/constants.py
+++ src/fastoad/module_management/constants.py
@@ -1,6 +1,6 @@
 """The place for module-level constants."""
 #  This file is part of FAST-OAD : A framework for rapid Overall Aircraft Design
-#  Copyright (C) 2020  ONERA & ISAE-SUPAERO
+#  Copyright (C) 2021 ONERA & ISAE-SUPAERO
 #  FAST is free software: you can redistribute it and/or modify
 #  it under the terms of the GNU General Public License as published by
 #  the Free Software Foundation, either version 3 of the License, or
@@ -12,7 +12,7 @@
 #  You should have received a copy of the GNU General Public License
 #  along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
-from enum import Enum
+from aenum import Enum
 
 # Services
 SERVICE_OPENMDAO_SYSTEM = "fast.openmdao.system"
