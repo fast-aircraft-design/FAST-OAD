b8d94a8266a5f1a7b3a1d401343e4a8515a89906
Scott Delbecq <scott.delbecq91@gmail.com>
2021-10-06 09:06:44 +0200

    Updated copyright date

--- src/fastoad/models/geometry/geom_components/ht/__init__.py
+++ src/fastoad/models/geometry/geom_components/ht/__init__.py
@@ -2,7 +2,7 @@
 Estimation of horizontal tail geometry (global)
 """
 #  This file is part of FAST-OAD : A framework for rapid Overall Aircraft Design
-#  Copyright (C) 2020  ONERA & ISAE-SUPAERO
+#  Copyright (C) 2021  ONERA & ISAE-SUPAERO
 #  FAST is free software: you can redistribute it and/or modify
 #  it under the terms of the GNU General Public License as published by
 #  the Free Software Foundation, either version 3 of the License, or
