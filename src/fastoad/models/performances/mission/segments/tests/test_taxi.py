974b1429732755724ada69593d5b36387255c8c7
Christophe DAVID <christophe.david@onera.fr>
2022-01-27 11:57:32 +0100

    Can now set target mass for taxi segment.

--- src/fastoad/models/performances/mission/segments/tests/test_taxi.py
+++ src/fastoad/models/performances/mission/segments/tests/test_taxi.py
@@ -40,3 +40,26 @@ def test_taxi():
     assert_allclose(last_point.mass, 49973.0, rtol=1e-4)
     assert_allclose(last_point.ground_distance, 5000.0)
     assert last_point.engine_setting == EngineSetting.IDLE
+
+
+def test_taxi_with_target_mass():
+    propulsion = FuelEngineSet(DummyEngine(0.5e5, 1.0e-5), 2)
+
+    segment = TaxiSegment(
+        target=FlightPoint(time=500.0, mass=50000.0),
+        propulsion=propulsion,
+        thrust_rate=0.1,
+        true_airspeed=10.0,
+        engine_setting=EngineSetting.IDLE,  # The engine model does not use this setting
+    )
+    flight_points = segment.compute_from(FlightPoint(altitude=10.0, time=10000.0))
+
+    start_point = flight_points.iloc[0]
+    last_point = flight_points.iloc[-1]
+    assert_allclose(last_point.altitude, 10.0, atol=1.0)
+    assert_allclose(last_point.time, 10500.0, rtol=1e-2)
+    assert_allclose(last_point.true_airspeed, 10.0, atol=0.1)
+    assert_allclose(start_point.mass, 50027.0, rtol=1e-4)
+    assert_allclose(last_point.mass, 50000.0, rtol=1e-4)
+    assert_allclose(last_point.ground_distance, 5000.0)
+    assert last_point.engine_setting == EngineSetting.IDLE
