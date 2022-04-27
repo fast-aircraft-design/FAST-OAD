02c7d1a71b5060fd9aa01fe0bfcf047d1c1b3740
Christophe DAVID <49477099+christophe-david@users.noreply.github.com>
2022-04-27 10:14:05 +0200

    Revised tests (more focused)

--- src/fastoad/openmdao/tests/test_implicit_component.py
+++ src/fastoad/openmdao/tests/test_implicit_component.py
@@ -17,9 +17,10 @@ These tests detect problems about doing deepcopy of a Problem that contains not 
 #  along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 import openmdao.api as om
+from numpy.testing import assert_allclose
 
-from fastoad._utils.testing import run_system
 from ..problem import FASTOADProblem
+from ..variables import VariableList
 
 
 class QuadraticComp(om.ImplicitComponent):
@@ -67,7 +68,10 @@ class QuadraticComp(om.ImplicitComponent):
         self.inv_jac = 1.0 / (2 * a * x + b)
 
 
-def test_implicit_component_openmdao():
+def test_variables_from_openmdao_problem():
+    # FASTOADProblem overloads run_model() and calls VariableList.from_problem().
+    # So a vanilla OpenMDAO problem is used here to ensure explicit calls
+    # to VariableList.from_problem()
 
     p = om.Problem()
 
@@ -79,15 +83,19 @@ def test_implicit_component_openmdao():
     p.model.nonlinear_solver = om.NewtonSolver(solve_subsystems=False)
     p.model.linear_solver = om.DirectSolver()
 
+    variables_1 = VariableList.from_problem(p, get_promoted_names=False)
+    assert_allclose(variables_1["quad.x"].value, 6.0)
+
     p.setup()
+    variables_2 = VariableList.from_problem(p, get_promoted_names=False)
+    assert_allclose(variables_2["quad.x"].value, 6.0)
 
     p.run_model()
+    variables_3 = VariableList.from_problem(p, get_promoted_names=False)
+    assert_allclose(variables_3["quad.x"].value, 3.0)
 
-    # sanity check: should sum to 3
-    print(p["quad.x"])
 
-
-def test_implicit_component_fast_oad():
+def test_fastoad_problem_run():
 
     p = FASTOADProblem()
 
@@ -102,46 +110,7 @@ def test_implicit_component_fast_oad():
     p.setup()
 
     p.run_model()
+    assert_allclose(p["quad.x"], 3.0)
 
-    # sanity check: should sum to 3
-    print(p["quad.x"])
-
-
-def test_implicit_component_fast_oad_no_newton():
-
-    p = FASTOADProblem()
-
-    p.model.add_subsystem("ivc_a", om.IndepVarComp("a", val=1.0), promotes=["*"])
-    p.model.add_subsystem("ivc_b", om.IndepVarComp("b", val=-4.0), promotes=["*"])
-    p.model.add_subsystem("ivc_c", om.IndepVarComp("c", val=3.0), promotes=["*"])
-    p.model.add_subsystem("quad", QuadraticComp(), promotes=["*"])
-
-    p.model.nonlinear_solver = om.NonlinearBlockGS()
-    p.model.linear_solver = om.DirectSolver()
-
-    p.setup()
-
-    p.run_model()
-
-    # sanity check: should sum to 3
-    print(p["quad.x"])
-
-
-def test_implicit_component_fast_oad_run_system():
-
-    ivc = om.IndepVarComp()
-    ivc.add_output(name="a", val=1.0)
-    ivc.add_output(name="b", val=-4.0)
-    ivc.add_output(name="c", val=3.0)
-
-    p = run_system(QuadraticComp(), ivc)
-
-    # sanity check: should sum to 3
-    print(p["x"])
-
-    # This one is gonna work because the run_system function uses an OpenMDAO problem and not a
-    # FASTOADProblem
-    p_2 = run_system(QuadraticComp(), ivc, add_solvers=True)
-
-    # sanity check: should sum to 3
-    print(p_2["x"])
+    variables = VariableList.from_problem(p)
+    assert_allclose(variables["x"].value, 3.0)
