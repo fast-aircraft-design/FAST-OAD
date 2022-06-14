e541df0185c002d1ad0d313ce1ddd995c98d563d
Florent LUTZ <66668699+florentLutz@users.noreply.github.com>
2022-06-14 16:17:48 +0200

    Update payload_range_file.py

--- src/fastoad/gui/payload_range/payload_range_file.py
+++ src/fastoad/gui/payload_range/payload_range_file.py
@@ -1,5 +1,5 @@
 """
-Defines the analysis and plotting functions for postprocessing
+Defines the analysis and plotting functions for postprocessing.
 """
 #  This file is part of FAST-OAD : A framework for rapid Overall Aircraft Design
 #  Copyright (C) 2021 ONERA & ISAE-SUPAERO
