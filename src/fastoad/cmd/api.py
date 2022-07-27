a893ac58aaf0fdd1bbeaf63925b5ec37661b377f
Scott Delbecq <scott.delbecq91@gmail.com>
2022-07-27 08:45:59 +0200

    Corrected multistart issue in api

--- src/fastoad/cmd/api.py
+++ src/fastoad/cmd/api.py
@@ -600,8 +600,12 @@ def _run_problem(
         problem.optim_failed = False  # Actually, we don't know
     else:
         optimization_options = conf._get_optimization_options()
-        if optimization_options is not None and "multistart" in optimization_options:
-            problem = conf._run_multistart(problem)
+        if optimization_options:
+            for optimization_option in optimization_options:
+                if optimization_option.get("multistart"):
+                    problem = conf._run_multistart(problem)
+                else:
+                    problem.optim_failed = problem.run_driver()
         else:
             problem.optim_failed = problem.run_driver()
     end_time = time()
