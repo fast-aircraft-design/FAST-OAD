ac0ca03bca0d5fb8a95a4a8e16360b883fef45c9
Scott Delbecq <scott.delbecq91@gmail.com>
2022-11-17 10:55:57 +0100

    Added warning for nan values when generating inputs

--- src/fastoad/io/configuration/configuration.py
+++ src/fastoad/io/configuration/configuration.py
@@ -20,7 +20,9 @@ import os.path as pth
 from abc import ABC, abstractmethod
 from importlib.resources import open_text
 from typing import Dict
+import warnings
 
+import numpy as np
 import openmdao.api as om
 import tomlkit
 from jsonschema import validate
@@ -230,8 +232,14 @@ class FASTOADProblemConfigurator:
         if source_file_path:
             ref_vars = DataFile(source_file_path, formatter=source_formatter)
             variables.update(ref_vars, add_variables=False)
+            nan_variable_names = []
             for var in variables:
                 var.is_input = True
+                if np.all(np.isnan(var.value)):
+                    nan_variable_names.append(var.name)
+            if nan_variable_names:
+                msg = "The following variables have NaN values: %s" % nan_variable_names
+                warnings.warn(msg)
         variables.save()
 
     def get_optimization_definition(self) -> Dict:
