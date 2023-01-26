bfafe3044c904b9a326a17db595ffcb517ea8c3e
Christophe DAVID <49477099+christophe-david@users.noreply.github.com>
2023-01-26 15:48:42 +0100

    Added "super-segment".
    [skip ci]

---
+++ src/fastoad/models/performances/mission/segments/takeoff/takeoff.py
@@ -0,0 +1,98 @@
+#  This file is part of FAST-OAD : A framework for rapid Overall Aircraft Design
+#  Copyright (C) 2023 ONERA & ISAE-SUPAERO
+#  FAST is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#  You should have received a copy of the GNU General Public License
+#  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+from dataclasses import dataclass, field
+
+from fastoad.constants import EngineSetting
+from fastoad.model_base import FlightPoint
+from fastoad.model_base.datacls import MANDATORY_FIELD
+from fastoad.model_base.propulsion import IPropulsion
+from ..base import DEFAULT_TIME_STEP, RegisteredSegment
+from ..end_of_takeoff import EndOfTakoffSegment
+from ..ground_speed_change import GroundSpeedChangeSegment
+from ..rotation import RotationSegment
+from ...base import FlightSequence
+from ...polar import Polar
+from ...polar_modifier import AbstractPolarModifier, LegacyPolar
+
+
+@dataclass
+class TakeOffSequence(RegisteredSegment, FlightSequence):
+    target: FlightPoint = MANDATORY_FIELD
+
+    #: A IPropulsion instance that will be called at each time step.
+    propulsion: IPropulsion = MANDATORY_FIELD
+
+    #: The Polar instance that will provide drag data.
+    polar: Polar = MANDATORY_FIELD
+
+    #:
+    polar_modifier: AbstractPolarModifier = LegacyPolar()
+
+    #: The reference area, in m**2.
+    reference_area: float = MANDATORY_FIELD
+
+    #: Used time step for computation (actual time step can be lower at some particular times of
+    #: the flight path).
+    time_step: float = DEFAULT_TIME_STEP
+
+    #: The EngineSetting value associated to the segment. Can be used in the
+    #: propulsion model.
+    engine_setting: EngineSetting = EngineSetting.CLIMB
+
+    rotation_equivalent_airspeed: float = MANDATORY_FIELD
+    _rotation_target: FlightPoint = field(default=FlightPoint(), init=False)
+
+    def __post_init__(self):
+        self._rotation_target = FlightPoint(equivalent_airspeed=self.rotation_equivalent_airspeed)
+        self.append(
+            GroundSpeedChangeSegment(
+                target=self._rotation_target,
+                propulsion=self.propulsion,
+                reference_area=120.0,
+                polar=self.polar,
+                polar_modifier=self.polar_modifier,
+                engine_setting=EngineSetting.CLIMB,
+                time_step=self.time_step,
+            )
+        )
+        self.append(
+            RotationSegment(
+                target=FlightPoint(),
+                propulsion=self.propulsion,
+                reference_area=120.0,
+                polar=self.polar,
+                polar_modifier=self.polar_modifier,
+                engine_setting=EngineSetting.CLIMB,
+                time_step=self.time_step,
+            )
+        )
+        self.append(
+            EndOfTakoffSegment(
+                target=self.target,
+                propulsion=self.propulsion,
+                reference_area=120.0,
+                polar=self.polar,
+                polar_modifier=self.polar_modifier,
+                engine_setting=EngineSetting.CLIMB,
+                time_step=0.05,
+            )
+        )
+
+    @property
+    def rotation_equivalent_airspeed(self) -> float:
+        return self._rotation_target.equivalent_airspeed
+
+    @rotation_equivalent_airspeed.setter
+    def rotation_equivalent_airspeed(self, value: float):
+        self._rotation_target.equivalent_airspeed = value
--- src/fastoad/models/performances/mission/segments/tests/test_takeoff_segments.py
+++ src/fastoad/models/performances/mission/segments/tests/test_takeoff_segments.py
@@ -1,5 +1,5 @@
 #  This file is part of FAST-OAD : A framework for rapid Overall Aircraft Design
-#  Copyright (C) 2021 ONERA & ISAE-SUPAERO
+#  Copyright (C) 2023 ONERA & ISAE-SUPAERO
 #  FAST is free software: you can redistribute it and/or modify
 #  it under the terms of the GNU General Public License as published by
 #  the Free Software Foundation, either version 3 of the License, or
@@ -21,6 +21,7 @@ from fastoad.model_base.propulsion import AbstractFuelPropulsion, FuelEngineSet
 from ..end_of_takeoff import EndOfTakoffSegment
 from ..ground_speed_change import GroundSpeedChangeSegment
 from ..rotation import RotationSegment
+from ..takeoff.takeoff import TakeOffSequence
 from ...polar import Polar
 from ...polar_modifier import AbstractPolarModifier, GroundEffectRaymer
 
@@ -119,7 +120,7 @@ def test_rotation(polar, polar_modifier):
     assert_allclose(last_point.time, 32.58, rtol=1e-2)
     assert_allclose(last_point.mass, 69995.0, rtol=1e-4)
     assert_allclose(last_point.ground_distance, 200.64, rtol=1e-3)
-    assert_allclose(last_point.alpha, 0.134, rtol=1e-2)
+    assert_allclose(np.degrees(last_point.alpha), 7.713, rtol=1e-3)
     assert last_point.engine_setting == EngineSetting.CLIMB
 
 
@@ -139,12 +140,13 @@ def test_end_of_takeoff(polar, polar_modifier):
     segment.time_step = 0.05
     flight_points = segment.compute_from(
         FlightPoint(
-            time=35.0,
+            time=32.58,
             altitude=0.0,
-            mass=70000.0,
+            mass=69995.0,
             true_airspeed=85.0,
             alpha=10.0 / 180 * 3.14,
             slope_angle=0.0,
+            ground_distance=200.64,
         )
     )  # Test with dict
 
@@ -158,4 +160,32 @@ def test_end_of_takeoff(polar, polar_modifier):
     assert_allclose(last_point.alpha, 0.105, rtol=1e-2)
     assert_allclose(last_point.slope_angle, 0.06907, rtol=1e-3)
     assert_allclose(last_point.slope_angle_derivative, 0.01251, rtol=1e-3)
+    assert last_point.engine_setting == EngineSetting.CLIMB
+
+
+def test_takeoff(polar, polar_modifier):
+    propulsion = FuelEngineSet(DummyEngine(1.0e5, 1.0e-5), 2)
+
+    # initialisation then change instance attributes
+    segment = TakeOffSequence(
+        target=FlightPoint(altitude=12.0),
+        propulsion=propulsion,
+        reference_area=120.0,
+        polar=polar,
+        polar_modifier=polar_modifier,
+        engine_setting=EngineSetting.CLIMB,
+        time_step=0.2,
+        rotation_equivalent_airspeed=75.0,
+    )
+    segment.thrust_rate = 1.0
+    segment.time_step = 0.05
+    flight_points = segment.compute_from(FlightPoint(altitude=0.0, mass=70000.0, true_airspeed=0.0))
+
+    last_point = flight_points.iloc[-1]
+    # Note: reference values are obtained by running the process with 0.01s as time step
+    assert_allclose(last_point.altitude, 12.0)
+    assert_allclose(last_point.true_airspeed, 90.88, rtol=1e-3)
+    assert_allclose(last_point.time, 38.46, rtol=1e-2)
+    assert_allclose(last_point.mass, 69993.0, rtol=1e-4)
+    assert_allclose(last_point.ground_distance, 302.97, rtol=1e-3)
     assert last_point.engine_setting == EngineSetting.CLIMB
