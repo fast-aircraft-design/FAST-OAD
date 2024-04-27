0ff16c02cf27d10e75487b0a2519404f4550c830
Christophe DAVID <christophe.david@onera.fr>
2024-04-27 17:57:31 +0200

    Added method to make a local run case

--- src/fastoad/__init__.py
+++ src/fastoad/__init__.py
@@ -1,5 +1,5 @@
 #  This file is part of FAST-OAD : A framework for rapid Overall Aircraft Design
-#  Copyright (C) 2022 ONERA & ISAE-SUPAERO
+#  Copyright (C) 2024 ONERA & ISAE-SUPAERO
 #  FAST is free software: you can redistribute it and/or modify
 #  it under the terms of the GNU General Public License as published by
 #  the Free Software Foundation, either version 3 of the License, or
