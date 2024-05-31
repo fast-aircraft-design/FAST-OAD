7a69b58f7d73d694ff689eed7997be1055283dad
Christophe DAVID <christophe.david@onera.fr>
2024-05-31 22:07:47 +0200

    Fixed copyrights

--- src/fastoad/cmd/cli.py
+++ src/fastoad/cmd/cli.py
@@ -37,19 +37,6 @@ from fastoad.module_management.exceptions import (
 )
 from . import api
 
-#  This file is part of FAST-OAD : A framework for rapid Overall Aircraft Design
-#  Copyright (C) 2022 ONERA & ISAE-SUPAERO
-#  FAST is free software: you can redistribute it and/or modify
-#  it under the terms of the GNU General Public License as published by
-#  the Free Software Foundation, either version 3 of the License, or
-#  (at your option) any later version.
-#  This program is distributed in the hope that it will be useful,
-#  but WITHOUT ANY WARRANTY; without even the implied warranty of
-#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#  GNU General Public License for more details.
-#  You should have received a copy of the GNU General Public License
-#  along with this program.  If not, see <https://www.gnu.org/licenses/>.
-
 NOTEBOOK_FOLDER_NAME = "FAST-OAD_notebooks"
 
 
