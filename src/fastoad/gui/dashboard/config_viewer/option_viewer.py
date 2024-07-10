903ccbc62e66fdd7b26bf0dcb10c2640388b4e18
Christophe DAVID <49477099+christophe-david@users.noreply.github.com>
2024-07-10 17:40:50 +0200

    test
    [skip ci]

--- src/fastoad/gui/dashboard/config_viewer/option_viewer.py
+++ src/fastoad/gui/dashboard/config_viewer/option_viewer.py
@@ -22,39 +22,25 @@ class OptionsParameter(ClassSelector):
         super().__init__(default=default, class_=om.OptionsDictionary, **params)
 
 
-class OptionsViewer(Viewer):
-    value = OptionsParameter(default=om.OptionsDictionary())
-
-    def __panel__(self):
-        widgets = [
-            self._get_option_view(name, definition) for name, definition in self.value._dict.items()
-        ]
-
-        return pn.Column(*[w for w in widgets if w])
-
-    def _get_option_view(self, name, option_definition):
-
-        if option_definition["deprecation"] is not None:
+class OptionWidgetFactory:
+    @classmethod
+    def get_option_viewer(cls, name, option_definition):
+        print(option_definition["deprecation"])
+        if option_definition["deprecation"] and option_definition["deprecation"][1]:
             return None
 
-        widget_kwargs = {"name": name}
-        widget_class = pn.widgets.TextInput
+        widget_class, widget_kwargs = cls._get_common_kwargs(name, option_definition)
 
-        if option_definition["val"] is _UNDEFINED:
-            widget_kwargs["value"] = None
-        elif option_definition["has_been_set"]:
-            widget_kwargs["value"] = option_definition["val"]
-        else:
-            widget_kwargs["value"] = option_definition["val"]
-
-        if option_definition["desc"]:
-            widget_kwargs["description"] = option_definition["desc"]
+        if option_definition["deprecation"]:
+            widget_kwargs["background"] = "gray"
+            widget_kwargs["description"] = option_definition["deprecation"][0]
 
         if option_definition["values"] is not None:
             widget_class = pn.widgets.Select
             widget_kwargs["options"] = option_definition["values"]
 
         elif option_definition["lower"] is not None and option_definition["upper"] is not None:
+            del widget_kwargs["description"]
             widget_kwargs["fixed_start"] = option_definition["lower"]
             widget_kwargs["fixed_end"] = option_definition["upper"]
 
@@ -74,12 +60,41 @@ class OptionsViewer(Viewer):
 
         return widget
 
+    @classmethod
+    def _get_common_kwargs(cls, name, option_definition):
+        widget_kwargs = {"name": name}
+        widget_class = pn.widgets.TextInput
+        if option_definition["val"] is _UNDEFINED:
+            widget_kwargs["value"] = None
+        elif option_definition["has_been_set"]:
+            widget_kwargs["value"] = option_definition["val"]
+        else:
+            widget_kwargs["value"] = option_definition["val"]
+        if option_definition["desc"]:
+            widget_kwargs["description"] = option_definition["desc"]
+        return widget_class, widget_kwargs
+
+
+class OptionsViewer(Viewer):
+    value = OptionsParameter(default=om.OptionsDictionary())
+
+    def __panel__(self):
+        widgets = [
+            OptionWidgetFactory.get_option_viewer(name, definition)
+            for name, definition in self.value._dict.items()
+        ]
+
+        return pn.Card(*[w for w in widgets if w], title="Component")
+
 
 options = om.OptionsDictionary()
 options.declare("toto", types=str, desc="test desc")
 options.declare("foo", values=["a", 1, True], desc="test desc")
-options.declare("deprecated", types=int, lower=0, upper=100, deprecation="tutu")
-options.declare("tutu", types=int, lower=0, upper=100)
-options.declare("truc", types=float, lower=-10.0, upper=100.0)
+options.declare(
+    "deprecated", types=int, lower=0, upper=100, deprecation="deprecated", desc="test desc"
+)
+options.declare("tutu", types=int, lower=0, upper=100, desc="test desc")
+options.declare("truc", types=float, lower=-10.0, upper=100.0, desc="test desc")
 print(options._dict)
+options["deprecated"] = 50
 ov = OptionsViewer(value=options).servable()
