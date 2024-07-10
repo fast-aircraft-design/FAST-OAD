314196b8359c53f255bec78b47fa63f723cc579b
Christophe DAVID <49477099+christophe-david@users.noreply.github.com>
2024-07-10 15:33:00 +0200

    test
    [skip ci]

--- src/fastoad/gui/dashboard/config_viewer/option_viewer.py
+++ src/fastoad/gui/dashboard/config_viewer/option_viewer.py
@@ -26,29 +26,60 @@ class OptionsViewer(Viewer):
     value = OptionsParameter(default=om.OptionsDictionary())
 
     def __panel__(self):
-        return pn.Column(
-            *[
-                self._get_option_view(name, definition)
-                for name, definition in self.value._dict.items()
-            ]
-        )
+        widgets = [
+            self._get_option_view(name, definition) for name, definition in self.value._dict.items()
+        ]
+
+        return pn.Column(*[w for w in widgets if w])
 
     def _get_option_view(self, name, option_definition):
-        # if self.definition.types is None:
-        if option_definition["val"] is _UNDEFINED:
-            value = None
-        else:
-            value = option_definition["val"]
 
-        widget = pn.widgets.TextInput(name=name, value=value)
+        if option_definition["deprecation"] is not None:
+            return None
+
+        widget_kwargs = {"name": name}
+        widget_class = pn.widgets.TextInput
+
+        if option_definition["val"] is _UNDEFINED:
+            widget_kwargs["value"] = None
+        elif option_definition["has_been_set"]:
+            widget_kwargs["value"] = option_definition["val"]
+        else:
+            widget_kwargs["value"] = option_definition["val"]
+
+        if option_definition["desc"]:
+            widget_kwargs["description"] = option_definition["desc"]
+
+        if option_definition["values"] is not None:
+            widget_class = pn.widgets.Select
+            widget_kwargs["options"] = option_definition["values"]
+
+        elif option_definition["lower"] is not None and option_definition["upper"] is not None:
+            widget_kwargs["fixed_start"] = option_definition["lower"]
+            widget_kwargs["fixed_end"] = option_definition["upper"]
+
+            try:
+                types = list(option_definition["types"])
+            except TypeError:
+                types = [option_definition["types"]]
+            if any(map(lambda t: issubclass(t, float), types)):
+                widget_class = pn.widgets.EditableFloatSlider
+            else:
+                widget_class = pn.widgets.EditableIntSlider
+
+            if widget_kwargs["value"] is None:
+                widget_kwargs["value"] = widget_kwargs["fixed_start"]
+
+        widget = widget_class(**widget_kwargs)
 
         return widget
 
 
-if __name__ == "__main__":
-    ov = OptionsViewer()
-    ov.value = om.OptionsDictionary()
-    ov.value.declare("toto", types=str)
-    ov.value.declare("tutu", types=int)
-    print(ov.value._dict)
-    pn.serve(ov)
+options = om.OptionsDictionary()
+options.declare("toto", types=str, desc="test desc")
+options.declare("foo", values=["a", 1, True], desc="test desc")
+options.declare("deprecated", types=int, lower=0, upper=100, deprecation="tutu")
+options.declare("tutu", types=int, lower=0, upper=100)
+options.declare("truc", types=float, lower=-10.0, upper=100.0)
+print(options._dict)
+ov = OptionsViewer(value=options).servable()
