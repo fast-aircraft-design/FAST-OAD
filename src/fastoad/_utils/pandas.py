639bfa321241d41870119053db68c43fd38bbc4d
Christophe DAVID <49477099+christophe-david@users.noreply.github.com>
2024-09-18 10:02:47 +0200

    Renaming
    (to please CodeClimate)

--- src/fastoad/_utils/pandas.py
+++ src/fastoad/_utils/pandas.py
@@ -22,17 +22,17 @@ from packaging.version import Version
 # DataFrame.applymap() is deprecated since pandas 2.1.0, in favor of DataFrame.map()
 # We get the correct one here once and for all.
 if Version(pd.__version__) >= Version("2.1"):
-    map_method = pd.DataFrame.map
+    MAP_METHOD = pd.DataFrame.map
 else:
-    map_method = pd.DataFrame.applymap
+    MAP_METHOD = pd.DataFrame.applymap
 
 
 # TODO: remove me when pandas <2.1 is no longer used.
 def apply_map(
-    df: pd.DataFrame, func: Callable, na_action: Optional[str] = None, **kwargs
+    dataframe: pd.DataFrame, func: Callable, na_action: Optional[str] = None, **kwargs
 ) -> pd.DataFrame:
     """
     Convenience function for using DataFrame.applymap or DataFrame.map
     according to pandas version.
     """
-    return map_method(df, func, na_action, **kwargs)
+    return MAP_METHOD(dataframe, func, na_action, **kwargs)
