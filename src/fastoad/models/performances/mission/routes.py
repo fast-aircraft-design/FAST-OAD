c73fe4d52e9caa6d59094f29d1904f3fec2f847f
Christophe DAVID <49477099+christophe-david@users.noreply.github.com>
2024-09-26 16:27:59 +0200

    Update src/fastoad/models/performances/mission/routes.py

--- src/fastoad/models/performances/mission/routes.py
+++ src/fastoad/models/performances/mission/routes.py
@@ -21,7 +21,6 @@ import numpy as np
 import pandas as pd
 from scipy.optimize import root_scalar
 
-from fastoad._utils.arrays import scalarize
 from fastoad.model_base import FlightPoint
 from fastoad.model_base.datacls import MANDATORY_FIELD
 from .base import FlightSequence, IFlightPart
@@ -75,7 +74,7 @@ class RangedRoute(FlightSequence):
 
     @cruise_distance.setter
     def cruise_distance(self, cruise_distance):
-        self.cruise_segment.target.ground_distance = scalarize(cruise_distance)
+        self.cruise_segment.target.ground_distance = np.asarray(cruise_distance).item()
         self.cruise_segment.target.set_as_relative("ground_distance")
 
     @property
