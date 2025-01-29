bacc09123e73a0fd51cd10fec8d81e92c7c041fa
Eric Nguyen Van <eric.nguyen_van@onera.fr>
2025-01-29 16:26:54 +0100

    Import optimization/sorting

--- src/fastoad/model_base/datacls.py
+++ src/fastoad/model_base/datacls.py
@@ -12,7 +12,7 @@
 #  You should have received a copy of the GNU General Public License
 #  along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
-from dataclasses import fields, dataclass
+from dataclasses import dataclass, fields
 
 #: To be put as default value for dataclass fields that should not have a default value.
 #: See :class:`BaseDataClass` for further information.
