06b3c114cfabfa43fa96629a2da057b611efab1e
enricostragiotti <44843509+enricostragiotti@users.noreply.github.com>
2025-08-19 12:47:13 +0200

    adding copyright to all *.py files

--- tests/memory_tests/test_multiple_runs.py
+++ tests/memory_tests/test_multiple_runs.py
@@ -1,3 +1,17 @@
+# This file is part of FAST-OAD : A framework for rapid Overall Aircraft Design
+# Copyright (c) 2025 ONERA & ISAE-SUPAERO
+# FAST-OAD is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+# SPDX-License-Identifier: GPL-3.0-or-later
+
 import gc
 import os.path as pth
 import tracemalloc
@@ -67,7 +81,7 @@ def test_runs(cleanup):
 
     count = 2
     for i in range(count):
-        print(f"RUN {i+1}/{count}")
+        print(f"RUN {i + 1}/{count}")
         run_problem()
 
     print_memory_state("Before garbage collector")
