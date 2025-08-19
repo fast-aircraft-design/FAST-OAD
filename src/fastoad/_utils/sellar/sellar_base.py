ea652f4683c6b49c489e1301c8ff5456e3ca82c4
enricostragiotti <44843509+enricostragiotti@users.noreply.github.com>
2025-08-19 12:44:22 +0200

    [skip ci] remove all copyrights

--- src/fastoad/_utils/sellar/sellar_base.py
+++ src/fastoad/_utils/sellar/sellar_base.py
@@ -6,18 +6,6 @@ SellarModel and SellarProblem should be used as-is.
 Providing different implementations of ISellarFactory will allow to provide
 the problem components in various ways
 """
-#  This file is part of FAST-OAD : A framework for rapid Overall Aircraft Design
-#  Copyright (C) 2024 ONERA & ISAE-SUPAERO
-#  FAST is free software: you can redistribute it and/or modify
-#  it under the terms of the GNU General Public License as published by
-#  the Free Software Foundation, either version 3 of the License, or
-#  (at your option) any later version.
-#  This program is distributed in the hope that it will be useful,
-#  but WITHOUT ANY WARRANTY; without even the implied warranty of
-#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#  GNU General Public License for more details.
-#  You should have received a copy of the GNU General Public License
-#  along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 import abc
 from dataclasses import dataclass
