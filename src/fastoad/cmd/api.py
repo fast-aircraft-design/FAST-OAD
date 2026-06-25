edeb3bf7de394ce4eeca0daa4ddd02d2f9a61a50
Scott Delbecq <scott.delbecq91@gmail.com>
2026-06-25 11:03:06 +0200

    Fix boolean masking for 'is_input' attribute in variable viewer to handle missing values and corrected copyright year

--- src/fastoad/cmd/api.py
+++ src/fastoad/cmd/api.py
@@ -803,7 +803,10 @@ def variable_viewer(
             columns={"name": "Name", "val": "Value", "units": "Unit", "desc": "Description"}
         )
         table["I/O"] = "OUT"
-        table.loc[table["is_input"] == True, "I/O"] = "IN"  # noqa: E712
+        # Some variables may have no "is_input" attribute (NaN/None), which would break
+        # boolean masking: treat any missing value as False (issue #314).
+        is_input_mask = table["is_input"].apply(lambda value: value is True)
+        table.loc[is_input_mask, "I/O"] = "IN"
         del table["is_input"]
         table.set_index("Name", drop=True, inplace=True)
 
